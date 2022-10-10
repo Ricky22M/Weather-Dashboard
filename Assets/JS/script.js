5dc59b48e1baf45a1ee98a63018ae9d3cf46756f
Ricky Esparza <RickyEsparza@Rickys-MacBook.local>
2022-10-10 14:42:50 -0600

    added a new function to JS

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -19,3 +19,48 @@ var uv = $('#uvIndex');
 // User can search for all current city name terms
 var allSearches;
 
+/* FUNCTIONS */
+
+// If saved local storage data already exists, then display in the "previouslySeached" array
+if (JSON.parse(localStorage.getItem("previousCities")) !== null) {
+    previouslySearched = JSON.parse(localStorage.getItem("previousCities"));
+}
+
+// Uses API key and 
+async function citySearch(city) {
+    //
+    requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.split(' ').join('+')},US&appid=${apiKey}&units=imperial`;
+    
+    await fetch(requestUrl)
+        .then(function (response) {
+            // Used if a city is invalid
+            if (response.status === 404) {
+                console.log("Unable to find selected city");
+                var asideEl = $('#leftContent');
+                var aside = $('<div>');
+                aside.attr("class", "alert alert-danger");
+                aside.attr("role", "alert");
+                aside.attr("id", "test");
+                aside.attr("Invalid city name");
+                asideEl.append(alert);
+            } else (response === 202) {
+                setInterval(function () {
+                    if (alert.css('opacity') < 0.1) {
+                        alert.remove();
+                        clearInterval();
+                    } else {
+                        alert.css('opacity', `${alert.css('opacity') - 0.1}`);
+                    }
+                }, 150);
+            }
+            return response.json();
+        })
+        // revise inital card
+        .then(function (data) {
+            // calls the "reviseCard" function, the "uvIndex" fucntion, and the "fiveDay" function
+            reviseCard(data.name, data.main.temp, data.main.humidity, data.weather[0].description);
+            uvIndex(data.coord.lat, data.coord.lon);
+            fiveDayCitySearch(data.name);
+        });
+}
+
